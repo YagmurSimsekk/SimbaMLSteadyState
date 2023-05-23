9bf5ed4f4e44fb91cb6345a9cc3e940cfc2f1746
Julian Zabbarov# <julian.zabbarov@student.hpi.de>
2023-05-23 10:45:42 +0200

    added missing line

--- tests/prediction/time_series/pipelines/synthetic_data_pipeline_test.py
+++ tests/prediction/time_series/pipelines/synthetic_data_pipeline_test.py
@@ -128,8 +128,6 @@ def test_synthetic_data_pipeline_export() -> None:
         )
     ).shape == (1, 2)
     assert pd.read_csv(
-        os.path.join(
-            os.getcwd(), export_path, "y_true-0.csv"
-        )
+        os.path.join(os.getcwd(), export_path, "y_true-0.csv")
     ).shape == (1, 2)
     shutil.rmtree(os.path.join(os.getcwd(), export_path))
